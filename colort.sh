5561ca447a28ab6ce8b81e65f164406e57c5c964
Tony Fiallos <afiallosh@gmail.com>
2019-08-15 22:14:38 +0200

    Build sed command separately

--- colort.sh
+++ colort.sh
@@ -2,16 +2,25 @@
 # Run like this: cat file.txt | ./color.sh WORD1 WORD2 WORD3 ...
 # Each WORD receives a different color in the log
 
+# TODO
+# [ ] Using echo -e removes tabs
+# [ ] Option to have more than one string per color
+# [ ] Handle no arguments passed
+# [ ] Faster?
+
 NC='\\e[0m' # No Color
 
+# Build sed command
+sedcmd=""
+idx=31 # RED
+for i; do
+    color='\\e['$idx'm'
+    sedcmd+="-e s/$i/$color$i$NC/g "
+    (( idx++ ))
+done
+
+# Replace each line in file
 while read line
 do
-    sedcmd=""
-    idx=31 # RED
-    for i; do
-	color='\\e['$idx'm'
-	sedcmd+="-e s/$i/$color$i$NC/g "
-	(( idx++ ))
-    done
     echo -e `echo $line | sed $sedcmd`
 done <&0 #read from stdin
